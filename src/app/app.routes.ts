8942c91df284fe7d305e560f411522df5d0736d5
Márcio José Mariano <marciojmariano@gmail.com>
2025-12-04 22:06:02 -0300

    Atualizar routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Home } from './pages/home/home';
 import { AlunoList } from './pages/alunos/list/list';
 import { AlunoCreate } from './pages/alunos/create/create';
 import { Alterar } from './pages/alunos/alterar/alterar';
+import { TransporteList } from './pages/transporte/list/list';
+import { TransporteCreate } from './pages/transporte/create/create';
+import { TransporteAlter } from './pages/transporte/alter/alter';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'alunos', component: AlunoList},
     {path: 'alunos/cadastro', component: AlunoCreate},
     {path: 'alunos/alterar/:id', component: Alterar},
-
+    {path: 'transporte', component: TransporteList},
+    {path: 'transporte/cadastro', component: TransporteCreate},
+    {path: 'transporte/alter/:id', component: TransporteAlter},
 ];
